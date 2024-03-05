Fix global metric calculation. Omit view interpolation.

dqn_tracker.py
@@ -135,6 +135,7 @@ class DQN(nn.Module):
         # convolution layers
         # TODO: variable n
         self.conv1 = nn.Conv3d(in_channels + 3, 16*n, 3, stride=2)
+        # self.conv1 = nn.Conv3d(in_channels, 16*n, 3, stride=2)
         self.norm1 = nn.BatchNorm3d(16*n)
         self.conv2 = nn.Conv3d(16*n, 32*n, 3, stride=2)
         self.norm2 = nn.BatchNorm3d(32*n)
@@ -318,16 +319,16 @@ class DQNModel():
                     # save global matching error
                     mae.append(torch.mean(torch.abs(env.bundle_density.data - env.true_density.data)))
                     # save global bending energy
+                    friction.append(torch.sum(torch.tensor([len(path) for path in env.finished_paths])))
                     bending_energy_ = []
-                    for i in range(len(env.finished_paths)):
-                        p0 = env.finished_paths[i][:-1]
-                        p1 = env.finished_paths[i][1:]
+                    for j in range(len(env.finished_paths)):
+                        p0 = env.finished_paths[j][:-1]
+                        p1 = env.finished_paths[j][1:]
                         segments = (p1 - p0) / env.step_size
                         energy = (torch.einsum('ij,ij->i', segments[1:], segments[:-1]) - 1.0) / -2.0
                         bending_energy_.append(torch.sum(energy))
                     bending_energy.append(torch.sum(torch.tensor(bending_energy_)))
                     # save global friction
-                    friction.append(torch.sum(torch.tensor([len(path) for path in env.finished_paths])))
                     
                     if show:
                         plot_durations(episode_durations)

image.py
@@ -53,7 +53,7 @@ class Image:
         # get amount of padding for each face
         zpad_top = zpad_btm = ypad_front = ypad_back = xpad_left = xpad_right = 0
 
-        radius = radius + 1 # leave one pixel to be cropped at the end to remove interpolation padding 
+        # radius = radius + 1 # leave one pixel to be cropped at the end to remove interpolation padding 
 
         if (i + radius) > shape[0]-1:
             zpad_btm = i + radius - (shape[0]-1)
@@ -73,13 +73,13 @@ class Image:
         # patch is data cropped around center. Note: slicing img creates a view (not a copy of img)
         patch = self.data[:, i-remainder[0]:i+remainder[1]+1, j-remainder[2]:j+remainder[3]+1, k-remainder[4]:k+remainder[5]+1]
 
-        if interp:
-            center = center.numpy().astype(np.float32)
-            remainder = remainder.reshape(3,2)
-            x = [np.arange(x-r[0], x+r[1]+1).astype(np.float32) for x,r in zip(np.round(center), remainder)]
-            x_ = [np.arange(x-r[0], x+r[1]+1).astype(np.float32) for x,r in zip(center, remainder)]
-            phii = np.stack(np.meshgrid(*x_, indexing='ij'))
-            patch = utils.interp(x, patch, phii, padding_mode=padding_mode) # after interp patch is a copy (not a view of data)
+        # if interp:
+        #     center = center.numpy().astype(np.float32)
+        #     remainder = remainder.reshape(3,2)
+        #     x = [np.arange(x-r[0], x+r[1]+1).astype(np.float32) for x,r in zip(np.round(center), remainder)]
+        #     x_ = [np.arange(x-r[0], x+r[1]+1).astype(np.float32) for x,r in zip(center, remainder)]
+        #     phii = np.stack(np.meshgrid(*x_, indexing='ij'))
+        #     patch = utils.interp(x, patch, phii, padding_mode=padding_mode) # after interp patch is a copy (not a view of data)
 
         if pad:
             patch_size = 2*radius+1
@@ -87,7 +87,7 @@ class Image:
             patch_[:, zpad_top:patch_size - zpad_btm, ypad_front:patch_size - ypad_back, xpad_left:patch_size - xpad_right] = patch
             patch = patch_
 
-        patch = patch[:, 1:-1, 1:-1, 1:-1]
+        # patch = patch[:, 1:-1, 1:-1, 1:-1]
 
         return patch, padding
 

load_data.py
@@ -80,7 +80,7 @@ def load_data(img_dir, label_file, pixelsize=[1.0,1.0,1.0], downsample_factor=1.
         density.draw_line_segment(s[:,:3], width=s[0,-1].item()/2)
 
     mask = torch.zeros_like(density.data)
-    mask[density.data>np.exp(-3)] = 1.0 
+    mask[density.data>np.exp(-3)] = 1.0
     mask = torch.tensor(dilation(mask, cube(10)[None]))
 
 